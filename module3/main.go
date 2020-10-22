cf60f5c442670c9c831c1eb6198ffecae323246b
Derek Roman <frank.d.roman@centene.com>
2020-10-22 11:14:22 -0500

    module 3

--- module3/main.go
+++ module3/main.go
@@ -2,16 +2,19 @@ package module3
 
 import (
 	"fmt"
-	"github.com/nathan-osman/go-sunrise"
 	"time"
+
+	"github.com/nathan-osman/go-sunrise"
 )
 
 func main() {
 	fmt.Println("hello world")
+
 	rise, set := sunrise.SunriseSunset(
 		43.65, -79.38, // Toronto, CA
 		2000, time.January, 1, // 2000-01-01
 	)
+
 	fmt.Printf("sunrise at: %v", rise)
 	fmt.Printf("sunset at: %v\n", set)
 }
